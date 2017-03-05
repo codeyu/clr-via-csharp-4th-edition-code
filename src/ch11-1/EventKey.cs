ad234e121e2b60a39c884213f47c9d30de00d361
codeyu <codeyu@gmail.com>
2017-03-05 12:21:27 +0800

    ch11-1

---
+++ src/ch11-1/EventKey.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace ch11_1
+{
+    // This class exists to provide a bit more type safety and 
+    // code maintainability when using EventSet
+    public sealed class EventKey : Object
+    {
+    }
+}
---
+++ src/ch11-1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ch11_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MailManager.Go();
+            TypeWithLotsOfEventsTest();
+        }
+        private static void TypeWithLotsOfEventsTest()
+        {
+            // The code here tests the event
+            TypeWithLotsOfEvents twle = new TypeWithLotsOfEvents();
+
+            // Add a callback here
+            twle.Foo += HandleFooEvent;
+            twle.SimulateFoo();
+            Console.WriteLine("The callback was invoked 1 time above" + Environment.NewLine);
+
+            // Add another callback here
+            twle.Foo += HandleFooEvent;
+            twle.SimulateFoo();
+            Console.WriteLine("The callback was invoked 2 times above" + Environment.NewLine);
+
+            // Remove a callback here
+            twle.Foo -= HandleFooEvent;
+            twle.SimulateFoo();
+            Console.WriteLine("The callback was invoked 1 time above" + Environment.NewLine);
+
+            // Remove another callback here
+            twle.Foo -= HandleFooEvent;
+            twle.SimulateFoo();
+            Console.WriteLine("The callback was invoked 0 times above" + Environment.NewLine);
+
+            Console.WriteLine("Press <Enter> to terminate this application.");
+            Console.ReadLine();
+        }
+
+        private static void HandleFooEvent(object sender, FooEventArgs e)
+        {
+            Console.WriteLine("Handling Foo Event here...");
+        }
+    }
+}
